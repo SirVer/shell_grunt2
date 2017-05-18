fb170d191d3bb203b09fdb6dd816185ed5eb3b0c
Holger Rapp <HolgerRapp@gmx.net>
2017-05-18 20:35:44 +0200

    Updated some dependencies.

--- watcher.lua
+++ watcher.lua
@@ -5,7 +5,7 @@ task = {
    redirect_stdout = "/tmp/build.out",
    redirect_stderr = "/tmp/build.err",
    should_run = function(path)
-      return path:ext() == "rs"
+      return path:ext() == "rs" or path:ext() == "toml" 
    end,
 }
 

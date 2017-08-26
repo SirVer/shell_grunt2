37947268d0c55feac4724800902467ff68136b29
Holger Rapp <HolgerRapp@gmx.net>
2017-08-26 12:10:10 +0200

    Handle CTRL-C properly.

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,11 @@ extern crate clap;
 extern crate notify;
 extern crate shell_grunt2;
 extern crate time;
+extern crate ctrlc;
 
 use notify::Watcher;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
 use shell_grunt2::task::{Runnable, RunningTask, Task};
 use shell_grunt2::lockfile;
 use std::time::Duration;
@@ -48,33 +51,48 @@ impl Task for ReloadWatcherFile {
 }
 
 fn watch_file_events(watcher_file: &str) {
-    // Ideally, the RecommendedWatcher would be owned by ShellGrunt2, but whenever I try that, the
-    // tool crashes whenever it should receive an event on the channel. So it needs to stay
-    // outside. :(
-    let (events_tx, events_rx) = mpsc::channel();
-    let mut watcher = notify::watcher(events_tx, Duration::from_millis(50)).unwrap();
-    watcher
-        .watch(&path::Path::new("."), notify::RecursiveMode::Recursive)
-        .unwrap();
+    let saw_interrupt_signal = Arc::new(AtomicBool::new(false));
+    let r = saw_interrupt_signal.clone();
+    ctrlc::set_handler(move || {
+        println!("Saw ctrl-c. Terminating!");
+        r.store(true, Ordering::SeqCst);
+    }).expect("Error setting Ctrl-C handler");
 
-    let (should_restart_tx, should_restart_rx) = mpsc::channel();
-    let mut tasks: Vec<Box<Task>> = vec![
-        Box::new(ReloadWatcherFile {
-            file_name: watcher_file.to_string(),
-            should_restart_tx,
-        }),
-    ];
-    for task in shell_grunt2::lua_task::run_file(path::Path::new(watcher_file)) {
-        tasks.push(task);
-    }
-    let mut shell_grunt2 = shell_grunt2::ShellGrunt2::new(&tasks, events_rx);
-    loop {
-        thread::sleep(std::time::Duration::from_millis(50));
-        match should_restart_rx.try_recv() {
-            Err(mpsc::TryRecvError::Empty) => {
-                shell_grunt2.spin();
+    'outer: loop {
+        println!("Watching file system with tasks from {}", watcher_file);
+
+        // Ideally, the RecommendedWatcher would be owned by ShellGrunt2, but whenever I try that, the
+        // tool crashes whenever it should receive an event on the channel. So it needs to stay
+        // outside. :(
+        let (events_tx, events_rx) = mpsc::channel();
+        let mut watcher = notify::watcher(events_tx, Duration::from_millis(50)).unwrap();
+        watcher
+            .watch(&path::Path::new("."), notify::RecursiveMode::Recursive)
+            .unwrap();
+
+        let (should_restart_tx, should_restart_rx) = mpsc::channel();
+        let mut tasks: Vec<Box<Task>> = vec![
+            Box::new(ReloadWatcherFile {
+                file_name: watcher_file.to_string(),
+                should_restart_tx,
+            }),
+        ];
+        for task in shell_grunt2::lua_task::run_file(path::Path::new(watcher_file)) {
+            tasks.push(task);
+        }
+        let mut shell_grunt2 = shell_grunt2::ShellGrunt2::new(&tasks, events_rx);
+
+        loop {
+            thread::sleep(std::time::Duration::from_millis(50));
+            if saw_interrupt_signal.load(Ordering::SeqCst) {
+                return;
             }
-            Ok(_) | Err(_) => break,
+            match should_restart_rx.try_recv() {
+                Err(mpsc::TryRecvError::Empty) => {
+                    shell_grunt2.spin();
+                }
+                Ok(_) | Err(_) => continue 'outer,
+            }
         }
     }
 }
@@ -104,8 +122,5 @@ fn main() {
         }
     };
 
-    loop {
-        println!("Watching file system with tasks from {}", watcher_file);
-        watch_file_events(watcher_file);
-    }
+    watch_file_events(watcher_file);
 }
